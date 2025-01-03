3f5118ccadab9b773d051ced533419303755b734
Adler Gonçalves <adlerhnrq@hotmail.com>
2025-01-03 16:38:12 -0300

    API - Fluent Validatio + Entity Framework

--- ChurchControl.API/Program.cs
+++ ChurchControl.API/Program.cs
@@ -8,7 +8,6 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
